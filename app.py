45a61af46aa90fbd54d4a465cb8e6af1d8c187ee
amir.daghighi <amiruyt@gmail.com>
2025-05-02 14:18:18 -0500

    slide bars added

--- app.py
+++ app.py
@@ -30,27 +30,21 @@ st.image(resized_image)
 col1, col2, col3, col4 = st.columns(4)
 col5, col6, col7, col8 = st.columns(4)
 
-# Input for amino acid mol ratio'
-amino_ratio = col1.number_input('Amino Acid mol ratio', max_value=30)
-
+amino_ratio = col1.slider('Amino Acid mol ratio', min_value=1, max_value=30, value=1)
 if amino_ratio < 1 or amino_ratio > 30:
     col5.error("Amino acid mol ratio must be between 0 and 30.")
 
-# Input for alcohol mol ratio
-alc_ratio = col2.number_input('Alcohol mol ratio', max_value=10)
-if alc_ratio < 1 or alc_ratio > 30:
-    col6.error("alcohol mol ratio must be between 1 and 10")
+alc_ratio = col2.slider('Alcohol mol ratio', min_value=1, max_value=10, value=1)
+if alc_ratio < 1 or alc_ratio > 10:
+    col6.error("Alcohol mol ratio must be between 1 and 10.")
 
-# Input for sugar
-sug_ratio = col3.number_input('Sugar mol ratio',max_value=10)
+sug_ratio = col3.slider('Sugar mol ratio', min_value=0, max_value=10, value=0)
 if sug_ratio < 0 or sug_ratio > 10:
-    col7.error("sugar mol ratio must be between 0 and 10")
+    col7.error("Sugar mol ratio must be between 0 and 10.")
 
-
-# Input for sugar
-water_fraction = col4.number_input('Water fraction',max_value=1.0)
+water_fraction = col4.slider('Water fraction', min_value=0.0, max_value=1.0, value=0.0, step=0.01)
 if water_fraction < 0 or water_fraction > 1.0:
-    col8.error("water fraction must be between 0 and 1")
+    col8.error("Water fraction must be between 0 and 1.")
 
 
 # Dropdown for 'PM' and 'DP'
@@ -74,7 +68,7 @@ sug_tox = sug_dict[sug_choice]
 
 ####
 # Toxicity prediction function 
-
+# which function should I choose?
 def calculate_weighted_tox(water_fraction, alcohol_ratio, sugar_ratio, amino_ratio, alcohol_tox, sugar_tox, amino_tox):
     components_fraction = 1- water_fraction
     total_ratio = alcohol_ratio + sugar_ratio + amino_ratio
@@ -86,6 +80,20 @@ def calculate_weighted_tox(water_fraction, alcohol_ratio, sugar_ratio, amino_rat
     
     return weighted_tox
 
-predicted_tox = calculate_weighted_tox(water_fraction, alc_ratio, 
+
+def calculate_weighted_tox2(water_fraction, alcohol_ratio, sugar_ratio, amino_ratio, alcohol_tox, sugar_tox, amino_tox):
+    components_fraction = 1- water_fraction
+    total_ratio = alcohol_ratio + sugar_ratio + amino_ratio
+    alcohol_weight = components_fraction * (alcohol_ratio / total_ratio)
+    sugar_weight = components_fraction * (sugar_ratio / total_ratio)
+    protein_weight = components_fraction * (amino_ratio / total_ratio)
+    
+    weighted_tox = (alcohol_weight * 1/alcohol_tox) + (sugar_weight * 1/sugar_tox) + (protein_weight * 1/amino_tox)
+    
+    return 1/weighted_tox
+
+
+
+predicted_tox = calculate_weighted_tox2(water_fraction, alc_ratio, 
                                        sug_ratio, amino_ratio, alc_tox, sug_tox, amino_tox)
 st.markdown(f'## the NADE toxicity is {round(predicted_tox, 2)} mg/kg')
